6f5d9048e10c3a4dc58adba9536613fc27c70f67
Judy Lin <judith.s.lin@gmail.com>
2015-06-26 14:46:04 -0600

    table of stories + add to icebox

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var unirest = require('unirest')
+var unirest = require('unirest');
+var project_id = process.env.PROJECT_ID;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-var url = "http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYTIMES_KEY'"
 
 unirest
-//can store NYT key in .env file to hide it from plain view.
-  .get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=c089e563b15ba06dc347af890f03e230:5:72386367')
+  .get('https://www.pivotaltracker.com/services/v5/projects/1374616/stories')
+  .headers('X-TrackerToken', process.env.PIVOTAL_TRACKER)
   .end(function (response) {
-    var titles = response.body.results.books.map(function (book) {
-      return book.title
-    });
     res.render('index', {
-      url: url,
-      title: 'express',
-      titles: titles
-    });
-  })
+      title: 'To-Do List',
+      response: response.body });
+  });
+});
+
+router.post('/', function(req, res, next) {
+  unirest
+    .post('https://www.pivotaltracker.com/services/v5/projects/1374616/stories')
+    .headers('X-TrackerToken', process.env.PIVOTAL_TRACKER)
+    .headers("Content-Type", "application/json")
+    .send({"name": req.body.new})
+    .end(function(response) {
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
